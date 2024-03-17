0e2f1feb4e8f8ec751672c9eb39134a002dbe79c
toydev <toy@ownway.info>
2024-03-17 17:44:54 +0900

    JsonUtility のダミー実装を追加した。

---
+++ OpenXRLib_UnityEngine.InputSystem/JsonUtility.cs
@@ -0,0 +1,9 @@
+﻿namespace UnityEngine.InputSystem
+{
+    internal class JsonUtility
+    {
+        public static T FromJson<T>(string json) => default(T);
+        public static string ToJson(object obj) => "";
+        public static string ToJson(object obj, bool prettyPrint) => "";
+    }
+}
