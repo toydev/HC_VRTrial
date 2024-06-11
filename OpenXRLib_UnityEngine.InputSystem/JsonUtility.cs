53ae14f38bbc3fbedfa3babafbb99d4313cf5db7
toydev <toy@ownway.info>
2024-06-11 21:21:11 +0900

    JsonUtility を再実装した。

--- OpenXRLib_UnityEngine.InputSystem/JsonUtility.cs
+++ OpenXRLib_UnityEngine.InputSystem/JsonUtility.cs
@@ -1,9 +1,46 @@
-﻿namespace UnityEngine.InputSystem
+﻿using Newtonsoft.Json;
+
+namespace UnityEngine.InputSystem
 {
     internal class JsonUtility
     {
-        public static T FromJson<T>(string json) => default(T);
-        public static string ToJson(object obj) => "";
-        public static string ToJson(object obj, bool prettyPrint) => "";
+        public static T FromJson<T>(string json)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+            catch (JsonException e)
+            {
+                Debug.LogError($"Json deserialization error: {e.Message}");
+                return default(T);
+            }
+        }
+
+        public static string ToJson(object obj)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(obj);
+            }
+            catch (JsonException e)
+            {
+                Debug.LogError($"Json serialization error: {e.Message}");
+                return string.Empty;
+            }
+        }
+
+        public static string ToJson(object obj, bool prettyPrint)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(obj, prettyPrint ? Formatting.Indented : Formatting.None);
+            }
+            catch (JsonException e)
+            {
+                Debug.LogError($"Json serialization error: {e.Message}");
+                return string.Empty;
+            }
+        }
     }
 }
