f138516bb374eeed5972dee6c9fc71eb521a1cf5
toydev <toy@ownway.info>
2024-06-23 20:59:07 +0900

    Fixed build error: Reimplement UnityEngine.JsonUtility class due to stripping.

---
+++ OpenXRLib_Common/JsonUtility.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+
+namespace UnityEngine
+{
+    public class JsonUtility
+    {
+        public static T FromJson<T>(string json)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+            catch (JsonException e)
+            {
+                Debug.LogError($"Json deserialization error: {e.Message}");
+                return default(T);
+            }
+        }
+
+        public static string ToJson(object obj)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(obj);
+            }
+            catch (JsonException e)
+            {
+                Debug.LogError($"Json serialization error: {e.Message}");
+                return string.Empty;
+            }
+        }
+
+        public static string ToJson(object obj, bool prettyPrint)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(obj, prettyPrint ? Formatting.Indented : Formatting.None);
+            }
+            catch (JsonException e)
+            {
+                Debug.LogError($"Json serialization error: {e.Message}");
+                return string.Empty;
+            }
+        }
+    }
+}
